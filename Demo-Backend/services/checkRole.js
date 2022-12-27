3f12541e6f4cb15b8f0565a5e756756e5dac4fda
Abhinand0514 <abhinandsunilk@gmail.com>
2022-12-27 15:09:30 +0530

    Solving error

--- Demo-Backend/services/checkRole.js
+++
@@ -1,7 +0,0 @@
-require("dotenv").config();
-
-function checkRole(req, res, next) {
-  if ((res.locals.role = process.env.USER)) res.sendstatus(401);
-  else next();
-}
-module.exports = { checkRole: checkRole };
